df31a205dfe86b60b1b1613eb697429fb4e9e549
Juliane <57350721+juliane99@users.noreply.github.com>
2020-07-13 14:37:48 +0200

    added nav

--- ueben/src/app/app-routing.module.ts
+++ ueben/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {Item2Component} from './item2/item2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', component: HomeComponent},
+{path: 'Item2', component: Item2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ueben/src/app/app.module.ts
+++ ueben/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Item2Component } from './item2/item2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Item2Component
   ],
   imports: [
     BrowserModule,
